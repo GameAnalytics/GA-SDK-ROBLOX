bef2c616649ea9d277c2b702a86122aeb12ee5d2
brinkokevin <kevinsarlah2@gmail.com>
2020-04-09 16:25:13 +0200

    Added initialization option to disable debug log

--- GameAnalyticsSDK/GameAnalyticsServerInit.server.lua
+++ GameAnalyticsSDK/GameAnalyticsServerInit.server.lua
@@ -8,20 +8,24 @@ local ServerStorage = game:GetService("ServerStorage")
 local GameAnalytics = require(ServerStorage.GameAnalytics)
 
 GameAnalytics:initialize({
-	build = "0.1",
+	build = "0.1";
 
-	gameKey = "",
-	secretKey = "",
+	gameKey = "";
+	secretKey = "";
 
-	enableInfoLog = true,
-	enableVerboseLog = false,
-	automaticSendBusinessEvents = true,
-	reportErrors = true,
+	enableInfoLog = true;
+	enableVerboseLog = false;
+
+	--debug is by default enabled in studio only
+	enableDebugLog = nil;
+
+	automaticSendBusinessEvents = true;
+	reportErrors = true;
 	
-	availableCustomDimensions01 = {},
-	availableCustomDimensions02 = {},
-	availableCustomDimensions03 = {},
-	availableResourceCurrencies = {},
-	availableResourceItemTypes = {},
+	availableCustomDimensions01 = {};
+	availableCustomDimensions02 = {};
+	availableCustomDimensions03 = {};
+	availableResourceCurrencies = {};
+	availableResourceItemTypes = {};
 	availableGamepasses = {};
 })
