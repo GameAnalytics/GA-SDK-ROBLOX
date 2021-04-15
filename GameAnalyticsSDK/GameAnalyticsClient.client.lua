6f3dfb61998dc1582d38264bba65c66ee83fdc4b
Lex T <lextoumbourou@gmail.com>
2021-04-15 22:39:52 +1000

    Formatting and comment fix

--- GameAnalyticsSDK/GameAnalyticsClient.client.lua
+++ GameAnalyticsSDK/GameAnalyticsClient.client.lua
@@ -8,14 +8,14 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Postie = require(ReplicatedStorage.Postie)
 local ScriptContext = game:GetService("ScriptContext")
 
-ScriptContext.Error:Connect(function(message , stackTrace , scriptInst)
+ScriptContext.Error:Connect(function(message, stackTrace, scriptInst)
 	if not scriptInst then
 		return
 	end
 
 	local scriptName = nil
 	local ok, _ = pcall(function()
-		scriptName = scriptInst:GetFullName()  -- CoreGui.RobloxGui.Modules.PlayerList error, can't get name because of security permission
+		scriptName = scriptInst:GetFullName()  -- Can't get name of some scripts because of security permission
 	end)
 	if not ok then
 		return
