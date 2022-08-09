85fa93ab79c8832142571fa22bef2113113ec0f9
xArshy <6077173+xArshy@users.noreply.github.com>
2022-08-09 11:43:25 +0400

    Update init.lua

--- gameanalytics-sdk/GameAnalytics/Store/init.lua
+++ gameanalytics-sdk/GameAnalytics/Store/init.lua
@@ -1,5 +1,6 @@
 local DS = game:GetService("DataStoreService")
 local RunService = game:GetService("RunService")
+local DSQ = require(script.DataStoreQueue)
 
 local store = {
 	PlayerDS = RunService:IsStudio() and {} or DS:GetDataStore("GA_PlayerDS_1.0.0"),
@@ -40,78 +41,76 @@ local store = {
 	EventsQueue = {},
 }
 
-function store:GetPlayerData(Player)
-	local PlayerData
-	local success = pcall(function()
-		PlayerData = RunService:IsStudio() and {} or (store.PlayerDS:GetAsync(Player.UserId) or {})
-	end)
+	function store:GetPlayerData(Player)
+		local key = Player.UserId	
+		local success, PlayerData = DSQ.AddRequest(key, function()			
+			return RunService:IsStudio() and {} or (store.PlayerDS:GetAsync(key) or {})
+		end, 7) -- Add to a queue with 7s delay between each request
 
-	if not success then
-		PlayerData = {}
+		if not success then
+			PlayerData = {}
+		end
+		return PlayerData
 	end
 
-	return PlayerData
-end
-
-function store:GetPlayerDataFromCache(userId)
-    local playerData = store.PlayerCache[tonumber(userId)]
-    if playerData then
-        return playerData
-    end
-    playerData = store.PlayerCache[tostring(userId)]
-    return playerData
-end
-
-function store:GetErrorDataStore(scope)
-	local ErrorDS
-	local success = pcall(function()
-		ErrorDS = RunService:IsStudio() and {} or DS:GetDataStore("GA_ErrorDS_1.0.0", scope)
-	end)
-
-	if not success then
-		ErrorDS = {}
+	function store:GetPlayerDataFromCache(userId)
+		local playerData = store.PlayerCache[tonumber(userId)]
+		if playerData then
+			return playerData
+		end
+		playerData = store.PlayerCache[tostring(userId)]
+		return playerData
 	end
 
-	return ErrorDS
-end
-
-function store:SavePlayerData(Player)
-
-	--Variables
-	local PlayerData = store:GetPlayerDataFromCache(Player.UserId)
-	local SavePlayerData = {}
-
-	if not PlayerData then
-		return
-	end
-
-	--Fill
-	for _, key in pairs(store.DataToSave) do
-		SavePlayerData[key] = PlayerData[key]
-	end
-
-	--Save
-	if not RunService:IsStudio() then
-		pcall(function()
-			store.PlayerDS:SetAsync(Player.UserId, SavePlayerData)
+	function store:GetErrorDataStore(scope)
+		local ErrorDS
+		local success = pcall(function()
+			ErrorDS = RunService:IsStudio() and {} or DS:GetDataStore("GA_ErrorDS_1.0.0", scope)
 		end)
-	end
-end
 
-function store:IncrementErrorCount(ErrorDS, ErrorKey, step)
-	if not ErrorKey then
-		return
-	end
+		if not success then
+			ErrorDS = {}
+		end
 
-	local count = 0
-	--Increment count
-	if not RunService:IsStudio() then
-		pcall(function()
-			count = ErrorDS:IncrementAsync(ErrorKey, step)
-		end)
+		return ErrorDS
 	end
 
-	return count
-end
+	function store:SavePlayerData(Player)
+		--Variables
+		local PlayerData = store:GetPlayerDataFromCache(Player.UserId)
+		local SavePlayerData = {}
 
+		if not PlayerData then
+			return
+		end
+
+		--Fill
+		for _, key in pairs(store.DataToSave) do
+			SavePlayerData[key] = PlayerData[key]
+		end
+
+		--Save
+		local key = Player.UserId
+		if not RunService:IsStudio() then
+			DSQ.AddRequest(key, function()
+				store.PlayerDS:SetAsync(key, SavePlayerData)
+			end, 7)
+		end
+	end
+
+	function store:IncrementErrorCount(ErrorDS, ErrorKey, step)
+		if not ErrorKey then
+			return
+		end
+
+		local count = 0
+		--Increment count
+		if not RunService:IsStudio() then
+			--Increment count
+			_, count = DSQ.AddRequest(ErrorKey, function()
+					return ErrorDS:IncrementAsync(ErrorKey, step)
+			end, 7)
+		end
+		return count
+	end
 return store
