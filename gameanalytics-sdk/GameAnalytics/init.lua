4ef9d641d3686dfb0fb9d14e7def56cc5e461f63
xArshy <6077173+xArshy@users.noreply.github.com>
2022-08-09 12:02:15 +0400

    Update init.lua

--- gameanalytics-sdk/GameAnalytics/init.lua
+++ gameanalytics-sdk/GameAnalytics/init.lua
@@ -614,6 +614,7 @@ function ga:PlayerRemoved(Player)
 			ga:endSession(Player.UserId)
 		else
 			store.PlayerCache[Player.UserId] = nil
+			store.DataStoreQueue.RemoveKey(Player.UserId)
 		end
 	end
 end
